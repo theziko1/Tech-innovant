cafbde78271019a79f75837f08a798f01babbdda
theziko1 <zakariaghachim96@gmail.com>
2024-02-20 11:38:50 +0100

    create home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
-import Image from "next/image";
+'use client'
+import React from 'react'
+import Theme from "@/components/theme";
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import { usePathname, useRouter } from 'next/navigation';
+import { Loader2 } from 'lucide-react';
 
-export default function Home() {
+
+
+function Page() {
+  const pathname = usePathname()
+  const {push}= useRouter()
+  if(pathname === "/") push("/pages/index.tsx")
   return (
-   <div>Hello Next</div>
-  );
+    <div className='flex h-screen w-screen items-center justify-center'>
+      <Loader2 className='animate-spin' />
+   
+    </div>
+  )
 }
+
+export default Page
