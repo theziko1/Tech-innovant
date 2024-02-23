ed377c4d70f43d70229b428800fff0fe5eca1f60
Zakaria Ghachim <98032256+theziko1@users.noreply.github.com>
2024-02-23 09:56:07 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
 'use client'
 import React from 'react'
-import { usePathname, useRouter } from 'next/navigation';
 import { Loader2 } from 'lucide-react';
 
 
 
 function Page() {
-  const pathname = usePathname()
-  const {push}= useRouter()
-  if(pathname === "/") push("/pages")
+
   return (
     <div className='flex h-screen w-screen items-center justify-center'>
       <Loader2 className='animate-spin' />
