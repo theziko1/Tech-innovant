3460c8f271a813fae871dac4f90c4211324cba79
theziko1 <zakariaghachim96@gmail.com>
2024-02-23 21:11:29 +0100

    final project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,9 @@ export default function RootLayout({
   
   return (
   <html lang="en">
+  <head>
+    <link rel="icon" href="/favicon.ico" sizes="any" />
+  </head>
   <body className={inter.className}>
   <ThemeProvider attribute="class"  defaultTheme="system"   enableSystem  disableTransitionOnChange >
         <main className="relative overflow-hidden">
