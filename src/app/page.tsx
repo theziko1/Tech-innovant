c1f57dc51b840112fc67527019fa2fdc3eb3f6ba
theziko1 <zakariaghachim96@gmail.com>
2024-02-24 21:44:39 +0100

    final project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@
 import React, { useEffect, useState } from 'react'
 import { Loader2 } from 'lucide-react';
 import Home from './pages/page';
+import Link from 'next/link';
+import Image from 'next/image';
 
 
 function Page() {
@@ -15,9 +17,28 @@ function Page() {
 
   }, []); 
   return (
-    <div className='flex h-screen w-screen items-center justify-center'>
+    <div className='flex w-full h-full items-center justify-center'>
 
-      { isLoading ? <Loader2 className='animate-spin' /> : (<Home/>)}
+      { isLoading ? 
+      <div className="bg-[#00BFA6] w-1/5 h-full rounded-xl border border-black my-40  p-8 m-8 text-white font-extrabold flex flex-col justify-center items-center">
+        <Loader2 className='animate-spin' />
+        <h4>Loading ...</h4>
+      </div> : (
+
+      <main className="container w-full  flex flex-col-reverse md:grid md:grid-cols-2 my-6 gap-16 ">
+        <div className="w-full p-4 md:p-0 justify-center items-center md:items-start flex flex-col gap-9">
+               <h3 className=" w-full font-['Kavoon'] text-5xl">Building <span className="text-[#00BFA6]">Brands</span> products and experiences</h3>
+                <h6 className="font-['ubuntu'] text-md">Let me help you with your projects</h6>
+                <Link href="/">
+                  <button  className="text-left hover:bg-transparent items-start bg-transparent text-[#00BFA4] underline">Contact me {`->`}</button>
+                </Link>
+
+        </div>
+        <div className="w-full justify-center items-center md:items-start flex flex-col gap-9" >
+          <Image width={450} height={450} src="/home.svg" alt="home" />
+        </div>
+      </main>
+      )}
       
     </div>
   )
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <>
     
-      <footer className="container mx-auto flex flex-col p-2 md:flex-row justify-center items-center bg-[#00BFA6] dark:bg-gray-500">Copyright &copy; 2024 &nbsp; <span className="text-white font-bold"> Tech Innovant. </span> &nbsp;All Rights Reserved</footer>
+      <footer className="container mx-auto flex flex-row p-2 justify-center items-center bg-[#00BFA6] dark:bg-gray-500">Copyright &copy; 2024 &nbsp; <span className="text-white font-bold"> Tech Innovant. </span> &nbsp;All Rights Reserved</footer>
     
     </>
     
